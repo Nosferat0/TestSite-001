238348612b10f5037535103a230c71cbe0351a96
NOS <nosferat0@gmail.com>
2012-09-30 18:09:09 +0200

    Index Ver.1A

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+* Simple Index page
+*
+* 
+*
+*/
+
+?>
+
+
+<html>
+<head><title>Index page ver.1 A</title></head>
+<body>
+<centre>
+<h1>Test Page Version: 1A</h1>
+<br />
+<img src="http://www.lab.com/logo1.png" />
+</centre>
+
+</body>
+
+</html>
+
